df896c8391236bb1e808cd2cb3cae955b3953877
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-04 21:08:22 +0000

    Made a random number guess

--- excersise3.py
+++ excersise3.py
@@ -9,3 +9,14 @@ print (1 == nine % 2)
 
 print (True and 2 == 4//2)
 
+print (True or False)
+
+import random
+
+random_number = random.randint(0,20)
+user_guess = input('Pick a number between 1 and 20?    ')
+
+if user_guess == random_number:
+    print ('You got it')
+else:
+    print ('Sorry better luck next time')
